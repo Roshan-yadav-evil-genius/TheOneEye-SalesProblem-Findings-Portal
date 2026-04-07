9c409d4e4d21eed62f6260b5880765ef69561eef
Roshan Yadav <roshan.yadav@12thwonder.com>
2026-04-07 21:13:46 +0530

    delay reduced

--- innovations_research.sh
+++ innovations_research.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 cd "$(dirname "$0")"
-timeout 50m /home/roshan-yadav/.local/bin/agent "$(cat innovations_research.md)" --yolo
+timeout 10m /home/roshan-yadav/.local/bin/agent "$(cat innovations_research.md)" --yolo
